027c0b1bfca6116484aacfe4a092ab0e0524d1d5
Jas James <jasdjames@gmail.com>
2020-12-17 21:24:50 -0600

    added JS to make the first question show

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+var Timer 
+var startQuiz
+var question1
+var question2
+var question3
+var question4
+var question5
+var directions
+
+
+
+
+
+
+
+
+// function clearSection(event) 
+// question1=document.getElementById(q1)
+// question1.addEventListener('click')
+
+// function startQuiz(event) {
+//     question1=document.getElementById("q1")
+//     if (question1.style.display === "none") {
+//         question1.style.display = "block";
+//     } else {
+//       question1.style.display = "none";
+//     }
+//   }
+
+function showQ1() {
+ startQuiz= document.getElementById("startClick")   
+question1 = document.getElementById("q1");
+question1.style.display ="block";
+startQuiz.style.display = "none";
+
+}
+
+if (
+
+    
+)
+    
+  
