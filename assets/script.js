7893423ecb63928f0dc387a3e1584698740ef818
Jas James <jasdjames@gmail.com>
2020-12-19 23:09:58 -0600

    added scoresheet

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,14 @@ var question2 = document.getElementById("q2");
 var question3 = document.getElementById("q3");
 var question4 = document.getElementById("q4");
 var question5 = document.getElementById("q5");
+var scoreSheet = document.getElementById("page2");
 var directions
 var timerEl = document.querySelector("#timer");
 var secondsLeft = 90
 var playerGuess
 // var correctAn = element.value="1";
-
+var timeTravel = document.querySelector(".btn-outline-primary").value
+var questions = document.querySelectorAll(".btn-outline-primary")
 var clickThruQ = ["clickQ2","clickQ3", "click4", "click5"];
 
 function showQ1() {
@@ -23,20 +25,32 @@ function showQ1() {
 
 
 
-
 function gameTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = `Timer: ${secondsLeft}`;
 
- if (secondsLeft === 0) {
+//  addEventListener("click",questions) ) 
+//     secondsLeft += 10;
+
+// } else { 
+//     secondsLeft -= 10; 
+// }
+if (secondsLeft === 0) {
             clearInterval(timerInterval);
             console.log(secondsLeft);
         }
     
     },1000)
-    
+    // Does not work but no errors
 
+if (timeTravel = 1){
+    timeTravel === true}
+ if (timeTravel === false) {
+     questions.onclick = function addTime() {
+         secondsLeft -= 10;
+     }
+ }
 // There is a loop that I could clean this up with
 
 }
@@ -65,6 +79,18 @@ function showQ3 (event){
  
  };
 
+ function showScore (event) {
+    question5.style.display = "none";
+    timerEl.style.display = "none";
+    scoreSheet.classList.remove("d-none");
+ }
+
+//  function addTime(){
+
+
+
+//  }
+
 
 //  clickThruQ = document.querySelector(".btn-outline-primary");
 //  clickThruQ.addEventListener("click",function clickThru(event) {
@@ -72,8 +98,6 @@ function showQ3 (event){
 //  clickThruQ = ([i].style.display ="block"); 
 //  }
 // })
-
- 
 
 
 
