1f82bb3af18f955de815dd4dc9aef774654d1158
Jas James <jasdjames@gmail.com>
2020-12-21 16:16:09 -0600

    formatted pages, added to readMe, screenshots

--- README.md
+++ README.md
@@ -1,24 +1,71 @@
 # jasdjames-codingQuiz-javascript-1
 
+## Description 
 
-Used Coolors for a color scheme 
+The purpose of this project was to create a coding quiz using things that I have learned in last month in coding bootcamp. The project asked 5 coding questions of the user. The project was fun and educational. I believe it defiantly raised morale and gave me a little more confidence in finding the solutions on my own. 
 
-https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp - Hide show using JS 
+## Installation
 
-https://codepen.io/nagasai/pen/KmmXez - Learning how to display none 
+This project is listed in the the GitHub repostitory at this is link [GitHub Repository] (https://github.com/jasdjames/jasdjames-password-generator-devlop-homework3) 
 
-Span id - Again after start game - ?/? 
-
-
-Colorsplosion Timer - Activity (Helpful)
+This page has been deployed here [Page] (https://jasdjames.github.io/jasdjames-password-generator-devlop-homework3/) 
 
 
-https://stackoverflow.com/questions/1947263/using-an-html-button-to-call-a-javascript-function -call onclick to function 
+## Usage 
+This quiz asks 5 coding based questions of the user. The quiz is timed. The quiz takes off 10 seconds if a question is missed. If time runs out the quiz jumps to the score page. The score is displayed and there is a place for the user to place their initials and the score. 
 
-https://stackoverflow.com/questions/3910736/how-to-call-multiple-javascript-functions-in-onclick-event - Do multiple things - one on click
 
-https://stackoverflow.com/questions/47383288/how-do-i-change-a-button-size-in-javascript - Set css to button
+Screenshots 
 
-https://stackoverflow.com/questions/3425291/javascript-if-statement-looking-through-an-array - conditionals with arrays 
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration - The loop was used from MDN 
+## Credits
+
+Will Utterback helped me with the time function as well as talked me through some of my some of my concerns about my code. Office hours after class was also very helpful. The Colorsplosion Timer may have been the most helpful activity 
+
+
+
+# References 
+
+[W3 Schools for Math logic] https://www.w3schools.com/jsref/jsref_obj_math.asp
+
+[W3Schools-Toggle] https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp - Hide show using JS 
+
+
+[Used Coolors for a color scheme] https://coolors.co/
+
+
+
+[Learning how to display none] https://codepen.io/nagasai/pen/KmmXez - e 
+
+[How to call onclick (one or multiple) to function] https://stackoverflow.com/questions/3910736
+
+[Set css to button] https://stackoverflow.com/questions/47383288/how-do-i-change-a-button-size-in-javascript - 
+
+[conditionals with arrays - Did not end uo using it but learned a lot ]https://stackoverflow.com/questions/3425291/javascript-if-statement-looking-through-an-array - 
+
+[This loop example was used in my quiz MDN] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration - 
+## License
+
+ MIT License
+
+Copyright (c) [2020] [JasDJames]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,11 @@ var question3 = document.getElementById("q3");
 var question4 = document.getElementById("q4");
 var question5 = document.getElementById("q5");
 var scoreSheet = document.getElementById("page2");
-// var directions
 var timerEl = document.querySelector("#timer");
 var secondsLeft = 90
-// var playerGuess
 var initials = document.getElementById("initials");
 var points = document.getElementById("score");
 var scoreNum = document.getElementById("scoreNum")
-// var correctAn = element.value="1";
 var page2 = document.getElementById("page2");
 var page3 = document.getElementById("page3");
 
@@ -24,7 +21,7 @@ var allAns = document.getElementsByClassName("btn-outline-primary")
 for (i = 0; i < allAns.length; i++) {
     allAns[i].addEventListener("click", selectAns)
 
-} 
+}
 scoreNum = 0;
 // Will Utterback assisted me with this function 
 function selectAns(event) {
@@ -41,24 +38,19 @@ function selectAns(event) {
         secondsLeft -= 10;
         scoreNum.innerHTML = points;
         alert("incorrect");
-        
+
     }
 
-var viewScore = document.getElementById("oldScoreHere")
+    var viewScore = document.getElementById("oldScoreHere")
 
-viewScore.innerHTML = `<p> Your score is ${scoreNum} </p>`;
-};        
+    viewScore.innerHTML = `<p> Your score is ${scoreNum} </p>`;
+};
 
 function showQ1() {
     question1.style.display = "block";
     startQuiz.style.display = "none";
 
 }
-
-
-// var Ans1 = allAns[3];
-
-
 function gameTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -68,140 +60,70 @@ function gameTime() {
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
             console.log(secondsLeft);
-        question1.style.display = "none";
-        question2.style.display = "none";
-        question3.style.display = "none";
-        question4.style.display = "none";
-        question5.style.display = "none";
-        scoreSheet.classList.remove("d-none");
+            question1.style.display = "none";
+            question2.style.display = "none";
+            question3.style.display = "none";
+            question4.style.display = "none";
+            question5.style.display = "none";
+            scoreSheet.classList.remove("d-none");
         }
 
     }, 1000)
-    // Does not work but no errors
 
-    // if (timeTravel = 1) {
-    //     timeTravel === true
-    // }
-    // if (timeTravel === false) {
-    //     questions.onclick = function addTime() {
-    //         secondsLeft -= 10;
-}
-//     }
-//     // There is a loop that I could clean this up with
-
-// }
-function showQ2(event) {
-    question1.style.display = "none";
-    question2.style.display = "block";
-   
-}
-
-
-
-
-
-
-
-
-
-
-
-function showQ3(event) {
-    question2.style.display = "none";
-    question3.style.display = "block";
-
-};
-
-function showQ4(event) {
-    question3.style.display = "none";
-    question4.style.display = "block";
-
-};
-
-function showQ5(event) {
-    question4.style.display = "none";
-    question5.style.display = "block";
-
-};
-
-function showScore(event) {
-    question5.style.display = "none";
-    timerEl.style.display = "none";
-    scoreSheet.classList.remove("d-none");
-    
-}
-
-
-
-// var viewScore = document.getElementById("oldScoreHere")
-
-// viewScore.innerHTML = `<p> Your score is ${scoreNum} </p>`;
-
-
-
-var buttonSubmit = document.getElementById("form-submit");
-buttonSubmit.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (buttonSubmit.value === "submit") {
-        // points.value = scoreNum;
-
-
-        localStorage.setItem("initials", initials.value);
-        localStorage.setItem("points", scoreNum);
-
-        
-        page3.innerHTML = `
-            <h2>Thank you for playing  ${localStorage.getItem("initials")} ! Your score of ${localStorage.getItem("points")} has been stored. Please play again!  </h2>`;
-        var playAgain = document.createElement("button");
-        playAgain.value = "play-again";
-        playAgain.textContent = "Play Again";
-        playAgain.style.margin = "2rem";
-        playAgain.classList.add("card-footer", "btn", "btn-outline-primary");
-        page3.appendChild(playAgain);
-        playAgain.addEventListener("click", function (event) {
-
-
-
-
-
-            location.reload();
-        })
-
-
+};    
+    function showQ2(event) {
+        question1.style.display = "none";
+        question2.style.display = "block";
 
     }
 
 
-});
+    function showQ3(event) {
+        question2.style.display = "none";
+        question3.style.display = "block";
+
+    };
+
+    function showQ4(event) {
+        question3.style.display = "none";
+        question4.style.display = "block";
+
+    };
+
+    function showQ5(event) {
+        question4.style.display = "none";
+        question5.style.display = "block";
+
+    };
+
+    function showScore(event) {
+        question5.style.display = "none";
+        timerEl.style.display = "none";
+        scoreSheet.classList.remove("d-none");
+
+    }
+    var buttonSubmit = document.getElementById("form-submit");
+    buttonSubmit.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (buttonSubmit.value === "submit") {
+            // points.value = scoreNum;
 
 
+            localStorage.setItem("initials", initials.value);
+            localStorage.setItem("points", scoreNum);
 
 
-//  function addTime(){
+            page3.innerHTML = `
+            <h2>Thank you for playing  ${localStorage.getItem("initials")} ! Your score of ${localStorage.getItem("points")} has been stored. Please play again!  </h2>`;
+            var playAgain = document.createElement("button");
+            playAgain.value = "play-again";
+            playAgain.textContent = "Play Again";
+            playAgain.style.margin = "2rem";
+            playAgain.classList.add("card-footer", "btn", "btn-outline-primary");
+            page3.appendChild(playAgain);
+            playAgain.addEventListener("click", function (event) {
+                location.reload();
+            })
+        }
+    });
 
-
-
-//  }
-
-
-//  clickThruQ = document.querySelector(".btn-outline-primary");
-//  clickThruQ.addEventListener("click",function clickThru(event) {
-// for (let i=0; i < clickThruQ.length; i++) {
-//  clickThruQ = ([i].style.display ="block"); 
-//  }
-// })
-
-
-
-// function clearSection(event) 
-// question1=document.getElementById(q1)
-// question1.addEventListener('click')
-
-// function startQuiz(event) {
-//     question1=document.getElementById("q1")
-//     if (question1.style.display === "none") {
-//         question1.style.display = "block";
-//     } else {
-//       question1.style.display = "none";
-//     }
-//   
