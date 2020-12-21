3058b1c19f78c822aa46737428e0b65fe40499b3
Jas James <jasdjames@gmail.com>
2020-12-21 12:52:17 -0600

    scoring works

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,8 @@ var initials = document.getElementById("initials");
 var points = document.getElementById("score");
 var scoreNum = document.getElementById("scoreNum")
 // var correctAn = element.value="1";
-
+var page2 = document.getElementById("page2");
+var page3 = document.getElementById("page3");
 
 var questions = document.querySelectorAll(".btn-outline-primary").value
 var clickThruQ = ["clickQ2", "clickQ3", "click4", "click5"];
@@ -23,8 +24,30 @@ var allAns = document.getElementsByClassName("btn-outline-primary")
 for (i = 0; i < allAns.length; i++) {
     allAns[i].addEventListener("click", selectAns)
 
-}
+} 
+scoreNum = 0;
+// Will Utterback assisted me with this function 
+function selectAns(event) {
+    var userClick = event.target.value;
+    console.log(event.target.value)
+    if (userClick === "2") {
+        alert("correct");
+        scoreNum += 10;
+        console.log(scoreNum);
+        alert(`Your score is ${scoreNum}`)
 
+    } else {
+
+        secondsLeft -= 10;
+        scoreNum.innerHTML = points;
+        alert("incorrect");
+        
+    }
+
+var viewScore = document.getElementById("oldScoreHere")
+
+viewScore.innerHTML = `<p> Your score is ${scoreNum} </p>`;
+};        
 
 function showQ1() {
     question1.style.display = "block";
@@ -33,7 +56,7 @@ function showQ1() {
 }
 
 
-var Ans1 = allAns[3];
+// var Ans1 = allAns[3];
 
 
 function gameTime() {
@@ -70,27 +93,9 @@ function gameTime() {
 function showQ2(event) {
     question1.style.display = "none";
     question2.style.display = "block";
-    scoreNum = 0;
+   
 }
 
-// Will Utterback assisted me with this function 
-function selectAns(event) {
-    var userClick = event.target.value;
-    console.log(event.target.value)
-    if (userClick === "2") {
-        alert("correct");
-        scoreNum += 10;
-        console.log(scoreNum);
-
-    } else {
-
-        secondsLeft -= 10;
-        scoreNum.innerHTML = points;
-        alert("incorrect");
-    }
-
-    
-};        
 
 
 
@@ -123,7 +128,15 @@ function showScore(event) {
     question5.style.display = "none";
     timerEl.style.display = "none";
     scoreSheet.classList.remove("d-none");
+    
 }
+
+
+
+var viewScore = document.getElementById("oldScoreHere")
+
+viewScore.innerHTML = `<p> Your score is ${scoreNum} </p>`;
+
 
 
 var buttonSubmit = document.getElementById("form-submit");
@@ -134,9 +147,9 @@ buttonSubmit.addEventListener("click", function (event) {
 
 
         localStorage.setItem("initials", initials.value);
-        localStorage.setItem("points", points.value);
+        localStorage.setItem("points", finalScore.value);
 
-        var page3 = document.getElementById("page3");
+        
         page3.innerHTML = `
             <h2>Thank you for playing  ${localStorage.getItem("initials")} ! Your score of ${localStorage.getItem("score")} has been stored. Please play again!  </h2>`;
         var playAgain = document.createElement("button");
